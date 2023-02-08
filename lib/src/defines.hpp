a65b69bc3f421d68ce967d74b530369fce412ad6
dyukoffv4 <net998art@gmail.com>
2023-02-08 21:46:12 +0300

    Initial

---
+++ lib/src/defines.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <exception>
+#include <stdexcept>
+
+namespace KP {
+    typedef std::vector<std::string> Args;
+    class Key;
+    class Terminal;
+}
