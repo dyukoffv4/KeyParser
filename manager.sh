99b316574ead8a8ee9d496f35e571fbdc9eb9ec8
dyukoffv4 <net998art@gmail.com>
2024-11-27 10:45:06 +0300

    add 'tests' command in manager.sh

--- manager.sh
+++ manager.sh
@@ -16,13 +16,20 @@ function Build {
     bash package_settings/build.sh
 }
 
+function Tests {
+    cd test
+    bash run.sh
+}
+
 function Clean {
-    if [[ $COMMAND_2 == "all" ]]; then rm -rf build; fi
+    if [[ $COMMAND_2 == "all" ]]; then rm -rf build tests/build tests/bin; fi
     if [[ $COMMAND_2 == "cmake" ]]; then rm -rf build/cmake; fi
     if [[ $COMMAND_2 == "build" ]]; then rm -rf build/keyparser; fi
+    if [[ $COMMAND_2 == "tests" ]]; then rm -rf tests/build tests/bin; fi
 }
 
 cd $CURRENT_PATH
 if [[ $COMMAND_1 == "cmake" ]]; then Cmake; fi
 if [[ $COMMAND_1 == "build" ]]; then Build; fi
+if [[ $COMMAND_1 == "tests" ]]; then Tests; fi
 if [[ $COMMAND_1 == "clean" ]]; then Clean; fi
