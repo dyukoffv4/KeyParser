bd6644fab476956976e85d9f01dca1d9854a24ca
dyukoffv4 <net998art@gmail.com>
2024-11-28 11:37:02 +0300

    Update tests

--- tests/src/key.cpp
+++ tests/src/key.cpp
@@ -3,15 +3,40 @@
 
 using namespace keyparser;
 
-TEST_CASE( "Key operators '==' and '!=' tests", "[key]" ) {
-    REQUIRE( Key('a') == Key('a') );
-    REQUIRE( Key('a') != Key('b') );
-    REQUIRE( Key('a') != Key("aa") );
-    REQUIRE( Key("aa") == Key("aa") );
-    REQUIRE( Key("aa") != Key("aab") );
-    REQUIRE( Key('a', "aa") != Key('a') );
-    REQUIRE( Key('a', "aa") != Key("aa") );
-    REQUIRE( Key('a', "aa") == Key('a', "aa") );
-    REQUIRE( Key('a', "aa") != Key('b', "aa") );
-    REQUIRE( Key('a', "aa") != Key('a', "aab") );
+TEST_CASE( "Key operators '==' tests", "[key]" ) {
+    CHECK( Key('a') == Key('a') );
+    CHECK( Key("aa") == Key("aa") );
+    CHECK( Key('a', "aa") == Key('a', "aa") );
+}
+
+TEST_CASE( "Key operators '!=' tests", "[key]" ) {
+    CHECK( Key('a') != Key('b') );
+    CHECK( Key('a') != Key("aa") );
+    CHECK( Key("aa") != Key("aab") );
+
+    CHECK( Key('a', "aa") != Key('a') );
+    CHECK( Key('a', "aa") != Key("aa") );
+    CHECK( Key('a', "aa") != Key('b', "aa") );
+    CHECK( Key('a', "aa") != Key('a', "aab") );
+}
+
+TEST_CASE( "Key operators '^=' tests", "[key]" ) {
+    bool result1 = Key('a') ^= Key('a');
+    bool result2 = Key('a') ^= Key('b');
+    bool result3 = Key('a', "aa") ^= Key('a');
+    bool result4 = Key('b', "aa") ^= Key('a');
+    bool result5 = Key('a', "aa") ^= Key('a', "aa");
+    bool result6 = Key('b', "aa") ^= Key('a', "aa");
+    bool result7 = Key('a', "ab") ^= Key('a', "aa");
+    bool result8 = Key('a', "ab") ^= Key('b', "aa");
+
+    CHECK( result1 );
+    CHECK( result3 );
+    CHECK( result5 );
+    CHECK( result6 );
+    CHECK( result7 );
+
+    CHECK_FALSE( result2 );
+    CHECK_FALSE( result4 );
+    CHECK_FALSE( result8 );
 }
---
+++ tests/src/parser.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+#include "src/parser.cpp"
+
+using namespace keyparser;
+
+TEST_CASE( "dumbParse check", "[parser]" ) {
+    SUCCEED();
+}
